4a4600a529f58790b8ac9dca9ae4908079be845e
Josh Cope <jcope2010@gmail.com>
2015-08-04 12:41:24 -0400

    update to latest ember-cli-moment-shim/remove import code from index.js

--- blueprints/ember-bootstrap-datetimepicker/index.js
+++ blueprints/ember-bootstrap-datetimepicker/index.js
@@ -1,13 +1,4 @@
 module.exports = {
   normalizeEntityName: function() {},
-
-  afterInstall: function() {
-    var that = this;
-
-    return this.addBowerPackageToProject('eonasdan-bootstrap-datetimepicker', '~4.14.30').then(function() {
-        return that.addBowerPackageToProject('moment').then(function() {
-          return that.addBowerPackageToProject('ember-cli-moment-shim');
-      });
-    });
-  }
+  afterInstall: function() {}
 };
