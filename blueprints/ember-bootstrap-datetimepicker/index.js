49e29c68e383bbc4b51b8cd6e390c265123c4e4b
Josh Cope <jcope2010@gmail.com>
2015-08-05 00:53:06 -0400

    fix merge conflict

--- blueprints/ember-bootstrap-datetimepicker/index.js
+++ blueprints/ember-bootstrap-datetimepicker/index.js
@@ -1,13 +1,9 @@
 module.exports = {
   normalizeEntityName: function() {},
-<<<<<<< HEAD
-  afterInstall: function() {}
-=======
 
   afterInstall: function() {
     return this.addAddonToProject('ember-cli-moment-shim', '~0.6.1').then(function() {
       return this.addBowerPackageToProject('eonasdan-bootstrap-datetimepicker', '~4.14.30');
     }.bind(this));
   }
->>>>>>> a3d4c9fff9d1b2b4fe41d5f70774d557c1b562e9
 };
